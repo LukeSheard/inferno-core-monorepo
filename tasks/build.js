a8d606e8562a12e1fd9f538d34b188c65c04d796
Luke Sheard <me@lukesheard.com>
2017-02-15 22:28:20 -0500

    Construct build task

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,9 @@
-const exec = require('child_process').exec;
+const spawn = require('child_process').exec;
+const { join } = require('path');
 const compilerOptions = require('../tsconfig.json').compilerOptions;
 
 
-const command = [ 'lerna exec --', 'tsc', '-d' ];
+const flags = [ 'lerna exec --concurrency 1 -- tsc', '-d' ];
 for (let i = 0; i < Object.keys(compilerOptions).length; i += 1) {
 	const option = Object.keys(compilerOptions)[i];
 	const optionValue = compilerOptions[option];
@@ -11,15 +12,32 @@ for (let i = 0; i < Object.keys(compilerOptions).length; i += 1) {
 	switch (typeof(optionValue)) {
 		case 'array': {
 			flagValue = optionValue.join(',');
+			break;
+		}
+		case 'boolean': {
+			if (flagValue) {
+				flagValue = '';
+			} else {
+				flagValue = false;
+			}
+			break;
 		}
 		default: {
 			flagValue = optionValue;
 		}
 	}
 
-	command.push(`--${option} ${flagValue}`)
+	flags.push(`--${option} ${flagValue}`)
 }
 
-command.push('src/**/*')
+flags.push('src/**/*')
 
-console.log(command.join(' '))
+const command = flags.join(' ');
+
+spawn(command, (err, stdout, stderr) => {
+	// console.log(stdout);
+
+	if (err) {
+		throw err;
+	}
+});
