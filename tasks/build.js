ccc2c44b3e5f9d9540a4d5c3d8e350a7d27ebb49
Luke Sheard <me@lukesheard.com>
2017-02-15 21:58:03 -0500

    Ignore build

--- tasks/build.js
+++ tasks/build.js
@@ -1,3 +1,25 @@
-const ts = require('typescript');
+const exec = require('child_process').exec;
+const compilerOptions = require('../tsconfig.json').compilerOptions;
 
-console.log(ts);
+
+const command = [ 'lerna exec --', 'tsc', '-d' ];
+for (let i = 0; i < Object.keys(compilerOptions).length; i += 1) {
+	const option = Object.keys(compilerOptions)[i];
+	const optionValue = compilerOptions[option];
+	
+	let flagValue;
+	switch (typeof(optionValue)) {
+		case 'array': {
+			flagValue = optionValue.join(',');
+		}
+		default: {
+			flagValue = optionValue;
+		}
+	}
+
+	command.push(`--${option} ${flagValue}`)
+}
+
+command.push('src/**/*')
+
+console.log(command.join(' '))
